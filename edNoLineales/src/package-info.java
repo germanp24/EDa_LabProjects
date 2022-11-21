6c3bf2fdf3a85dacbde5779ee1188026c7f11a9f
victorcentellas <victor.centellas@alu.uclm.es>
2022-11-21 17:46:16 +0100

    Esqueleto

---
+++ edNoLineales/src/package-info.java
@@ -0,0 +1,4 @@
+/**
+* This package provides the interfaces and classes needed to work with simple graphs. It is intended as a didactic tool for the
+* Data Structures course taught at the Escuela Superior de Informática of the Castilla-La Mancha University.
+*/
